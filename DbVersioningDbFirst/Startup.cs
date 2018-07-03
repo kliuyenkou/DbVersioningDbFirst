ccdc7447772a32c6ee9f2f4ff6fb04ba81a58315
Viktar Kliuyenkou <klyuenkov@gmail.com>
2018-07-03 17:14:09 +0300

    Initial commit

---
+++ DbVersioningDbFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DbVersioningDbFirst.Startup))]
+namespace DbVersioningDbFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
